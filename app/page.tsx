93b9f1a4f20e484e408959b3d5032189d6cd099a
Pedro Uziel Barrita Licea <leibarrita@gmail.com>
2024-08-12 16:51:52 -0600

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Header from "@/components/header/Header";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Header></Header>
-      Hello World
-    </main>
-  );
+  return <main className={styles.main}>Hello World</main>;
 }
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
 import styles from "./header.module.css";
 
 const Header = () => {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={`${styles.header} ${styles.separation}`}>
+      <Link href="/">Uziel Barrita</Link>
+      <div className={styles.separation}>
+        <Link href="portafolio">Portafolio</Link>
+        <Link href="contact">Contact</Link>
+        <Link href="">Spanish</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
