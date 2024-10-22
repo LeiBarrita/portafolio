924125c419daf74cdb25067ee6906edc610f1a53
Pedro Uziel Barrita Licea <leibarrita@gmail.com>
2024-10-22 10:29:33 -0600

    Image slider slidding the images YuY

--- components/imgSlider/ImgSlider.tsx
+++ components/imgSlider/ImgSlider.tsx
@@ -1,6 +1,7 @@
 import { ListOfAppPhotos } from "@/types";
 import styles from "./imgSlider.module.css";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const getDriveThumbnailUrl = (drivelink: string): string => {
   const regex = /\/file\/d\/(.*?)\/view\?/;
@@ -13,12 +14,25 @@ const getDriveThumbnailUrl = (drivelink: string): string => {
 };
 
 const ImgSlider = ({ images }: { images: ListOfAppPhotos }) => {
+  const [currentImg, setCurrentImg] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImg((prevImg) =>
+        prevImg === images.length - 1 ? 0 : prevImg + 1
+      );
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Image
       className={styles.image}
       width={100}
       height={100}
-      src={getDriveThumbnailUrl(images[0].link)}
+      src={getDriveThumbnailUrl(images[currentImg].link)}
       alt="Project picture"
     />
   );
