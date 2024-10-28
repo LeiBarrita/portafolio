5fd23159c9322e17e226e3247bf748440d8a25d4
Pedro Uziel Barrita Licea <leibarrita@gmail.com>
2024-10-28 14:40:37 -0600

    BentoGrid working

--- components/bentoGrid/BentoGrid.tsx
+++ components/bentoGrid/BentoGrid.tsx
@@ -4,29 +4,41 @@ import Image from "next/image";
 import React from "react";
 
 const ImgBentoGrid = ({ images }: { images: ListOfAppPhotos }) => {
+  const splitImageArray = (
+    baseArray: ListOfAppPhotos,
+    noCols: number
+  ): ListOfAppPhotos[] => {
+    const imageArrays: ListOfAppPhotos[] = Array.from(
+      { length: noCols },
+      () => []
+    );
+
+    baseArray.forEach((item, index) => {
+      const currentCol = index % noCols;
+      imageArrays[currentCol].push(item);
+    });
+
+    return imageArrays;
+  };
+
   return (
     <div className={styles.bentoGrid}>
-      {images.map((image, index) => (
-        <div key={image.id} className={styles.bentoGridItem}>
-          <Image
-            key={image.id}
-            // className={styles.bentoGridImage}
-            width={100}
-            height={100}
-            src={image.link}
-            alt="Project picture"
-            onLoadingComplete={({ naturalWidth, naturalHeight }) => " "}
-          />
+      {splitImageArray(images, 3).map((imageArray, index) => (
+        <div key={index} className={styles.bentoGridColumn}>
+          {imageArray.map((image) => (
+            <Image
+              key={image.id}
+              className={styles.bentoGridImage}
+              width={100}
+              height={100}
+              src={image.link}
+              alt="Project picture"
+            />
+          ))}
         </div>
       ))}
     </div>
   );
 };
-
-// const ImgBentoGrid = () => {
-//   return (
-//     <div>ImgBentoGrid</div>
-//   )
-// }
 
 export default ImgBentoGrid;
