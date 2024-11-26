c7a21914da4ab468f6a16aa19dbb8d407cf5874a
Pedro Uziel Barrita Licea <leibarrita@gmail.com>
2024-11-26 12:31:09 -0600

    Experience logos loading skeleton

--- components/skeleton/Skeleton.tsx
+++ components/skeleton/Skeleton.tsx
@@ -2,13 +2,19 @@ import React, { ReactNode } from "react";
 import styles from "./skeleton.module.css";
 
 function Skeleton({
+  className,
   children,
   loading,
 }: {
+  className?: string;
   children: ReactNode;
   loading: boolean;
 }) {
-  return <div className={`${loading && styles.skeleton}`}>{children}</div>;
+  return (
+    <div className={`${loading && styles.skeleton} ${className}`}>
+      {children}
+    </div>
+  );
 }
 
 export default Skeleton;
