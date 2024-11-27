19f43840babf561241adf5f3df9d6dac63f42f3b
Pedro Uziel Barrita Licea <leibarrita@gmail.com>
2024-11-27 12:41:21 -0600

    anchor to Link

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,25 +1,25 @@
-import a from "next/link";
+import Link from "next/link";
 import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={`${styles.footer} secondary-section`}>
-      <a className="hover-shine hover-grow" href="/">
+      <Link className="hover-shine hover-grow" href="/">
         Home
-      </a>
-      <a className="hover-shine hover-grow" href="/experience">
+      </Link>
+      <Link className="hover-shine hover-grow" href="/experience">
         Experience
-      </a>
-      <a className="hover-shine hover-grow" href="/#home-contact">
+      </Link>
+      <Link className="hover-shine hover-grow" href="/#home-contact">
         Contact
-      </a>
-      <a className="hover-shine hover-grow" href="/credits">
+      </Link>
+      <Link className="hover-shine hover-grow" href="/credits">
         Credits
-      </a>
-      {/* <a  className="hover-shine hover-grow" href="/">Spanish</a> */}
-      <a className="hover-shine hover-grow" href="https://g.co/kgs/iiJS65j">
+      </Link>
+      {/* <Link  className="hover-shine hover-grow" href="/">Spanish</Link> */}
+      <Link className="hover-shine hover-grow" href="https://g.co/kgs/iiJS65j">
         Michoacán, México
-      </a>
+      </Link>
     </footer>
   );
 };
