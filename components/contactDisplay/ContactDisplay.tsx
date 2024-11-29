b21469964b275b603044bdaa901e69b5d6676f9b
Pedro Uziel Barrita Licea <leibarrita@gmail.com>
2024-11-29 13:38:56 -0600

    contact new animation

--- components/contactDisplay/ContactDisplay.tsx
+++ components/contactDisplay/ContactDisplay.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { CSSProperties, useEffect, useState } from "react";
 import styles from "./contactDisplay.module.css";
 import ContactIcon from "../contactIcon/ContactIcon";
-import { ListOfContactInfo } from "@/types";
+import { ContactInfo, ListOfContactInfo } from "@/types";
 
 const ContactDisplay = ({
   contactList,
@@ -11,18 +11,53 @@ const ContactDisplay = ({
   return (
     <div className={`${styles.contactList}`}>
       {contactList.map((contact) => (
-        <div key={contact.id} className={styles.contactItem}>
-          <ContactIcon
-            contactInfo={contact}
-            className={`${styles.icon} scroll-slide-right`}
-          />
-          <h4>{contact.rawLink}</h4>
-          {/* <div className={styles.hoverLink}>
-            <ContactIcon contactInfo={contact} />
-            <h4 className="scroll-slide-right">{contact.rawLink}</h4>
-          </div> */}
-        </div>
+        <Contact key={contact.id} contact={contact} />
       ))}
+    </div>
+  );
+};
+
+const Contact = ({ contact }: { contact: ContactInfo }) => {
+  const [linkLenght, setLinkLenght] = useState(0);
+  // const [textStyle, setTextStyle] = useState<CSSProperties>();
+
+  // type CustomCSSProperties = React.CSSProperties & {
+  //   "--hover-width"?: string;
+  // };
+
+  const h4Style = {
+    h4: { transition: "width " },
+  };
+
+  function getTextWidth(text: string): number {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+
+    if (context) {
+      context.font = getComputedStyle(document.body).font;
+      return Math.ceil(context?.measureText(text).width);
+    }
+    return 0;
+  }
+
+  useEffect(() => {
+    setLinkLenght(getTextWidth(contact.rawLink));
+    // const textLenght = getTextWidth(contact.rawLink);
+    // const hoverStyle: CustomCSSProperties = {
+    //   "--hover-width": `${textLenght}px`,
+    // };
+    // setTextStyle(hoverStyle);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className={styles.contactItem}>
+      <ContactIcon
+        contactInfo={contact}
+        className={`${styles.icon} scroll-slide-right`}
+      />
+      <h4>{contact.rawLink}</h4>
+      {/* {linkLenght} */}
     </div>
   );
 };
